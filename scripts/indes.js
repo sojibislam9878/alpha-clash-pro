87f90669cf8b18407387d0c30b9f5763580379ee
sojibhasan9878 <sojibhasan9878@gmail.com>
2024-02-14 17:51:23 +0600

    random letter display done

--- scripts/indes.js
+++ scripts/indes.js
@@ -1,7 +1,19 @@
 const playBtn = document.getElementById("playbtn");
 const home = document.getElementById("home")
 const playGround = document.getElementById("playGround");
+const display = document.getElementById("display")
+const letterString ="abcdefghijklmnopqrstuvwxyz"
+const letterArray = letterString.split("")
+const index = Math.floor(Math.random()*25)
+const letter = letterArray[index]
+
+
+display.innerText = letter
+
 playBtn.addEventListener("click",function(){
     home.classList.add("hidden")
     playGround.classList.remove("hidden")
 })
+
+
+
